6a63ef33021963ae6a1b5f30efc24d54ac17d4c0
CHIHAOUI WAED <148530537+chihaouiwaed@users.noreply.github.com>
2025-03-10 00:08:39 +0100

    Update angry.ps1

--- angry.ps1
+++ angry.ps1
@@ -1,1 +1,20 @@
-Invoke-WebRequest -Uri "https://www.mediafire.com/file/owgnox66mxdjkkl/bypasse_2_2.7z/file" -OutFile "bypasse_2_2.7z"; 7z x bypasse_2_2.7z; 7z x bypasse_2.7z; 7z x bypasse.7z
+# URL of the file to download
+$url = "https://www.mediafire.com/file/owgnox66mxdjkkl/bypasse_2_2.7z/file"
+$outputFile = "bypasse_2_2.7z"
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+
+# Path to 7z executable (ensure 7-Zip is installed and in PATH or specify the full path)
+$sevenZip = "7z"
+
+# First extraction
+& $sevenZip x $outputFile -o"bypasse_2_2" -y
+
+# Second extraction
+& $sevenZip x ".\bypasse_2_2\bypasse_2.7z" -o"bypasse_2" -y
+
+# Third extraction
+& $sevenZip x ".\bypasse_2\bypasse.7z" -o"bypasse" -y
+
+Write-Host "Extraction completed successfully."
